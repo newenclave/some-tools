2d09d18029deaf555d45d60e3bb54c64e5a23433
k.v <pikrat.acid@gmail.com>
2013-10-29 18:30:11 +0400

    mem array

--- memory/mem-array.c
+++ memory/mem-array.c
@@ -7,11 +7,10 @@ struct mem_array_data {
     size_t                 element_size_;
 };
 
-
 struct mem_array_data *mem_array_new2( size_t count, size_t element_size )
 {
     struct mem_array_data *new_data =
-            (mem_array_data *)malloc( sizeof( struct mem_array_data ) );
+            (struct mem_array_data *)malloc( sizeof( struct mem_array_data ) );
     if( NULL == new_data )
         return NULL;
     new_data->mem_ = mem_block_new( count * element_size );
@@ -21,12 +20,53 @@ struct mem_array_data *mem_array_new2( size_t count, size_t element_size )
         return NULL;
     }
     new_data->element_size_ = element_size;
+    return new_data;
 }
 
 struct mem_array_data *mem_array_new( size_t element_size )
 {
-
+    return mem_array_new2( 0, element_size );
 }
 
 
-int mem_block_free(struct mem_block_data *mb);
+int mem_array_free( struct mem_array_data *mar )
+{
+    if( NULL != mar ) {
+        mem_block_free( mar->mem_ );
+        free( mar );
+    }
+    return 1;
+}
+
+
+void  *mem_array_at( struct mem_array_data *mar, size_t element_index )
+{
+    char *d = mem_block_data( mar->mem_ );
+    return (d + (element_index * mar->element_size_));
+}
+
+size_t mem_array_size( struct mem_array_data *mar )
+{
+    return (mem_block_size( mar->mem_ ) / mar->element_size_);
+}
+
+int mem_array_push_back2( struct mem_array_data *mar, void *element, size_t count )
+{
+    return mem_block_concat( mar->mem_, element, count * mar->element_size_ );
+}
+
+int mem_array_push_back( struct mem_array_data *mar, void *element )
+{
+    return mem_array_push_back2( mar, element, 1 );
+}
+
+int mem_array_resize( struct mem_array_data *mar, size_t new_count )
+{
+    return mem_block_resize2( mar->mem_, new_count * mar->element_size_, 0 );
+}
+
+int mem_array_reserve( struct mem_array_data *mar, size_t count )
+{
+    return mem_block_reserve( mar->mem_, count * mar->element_size_ );
+}
+
--- memory/mem-array.h
+++ memory/mem-array.h
@@ -1,10 +1,20 @@
 #if !defined(MEMARRAY_20132910_H)
 #define MEMARRAY_20132910_H
 
-struct mem_array_data;
+#include <stdlib.h>
 
+struct mem_array_data;
 
 struct mem_array_data *mem_array_new( size_t element_size );
 struct mem_array_data *mem_array_new2( size_t count, size_t element_size );
+int                    mem_array_free( struct mem_array_data *mar );
+
+void  *mem_array_at( struct mem_array_data *mar, size_t element_index );
+size_t mem_array_size( struct mem_array_data *mar );
+int    mem_array_push_back( struct mem_array_data *mar, void *element );
+int    mem_array_push_back2( struct mem_array_data *mar, void *element, size_t count );
+int    mem_array_resize( struct mem_array_data *mar, size_t new_count );
+int    mem_array_reserve( struct mem_array_data *mar, size_t count );
+
 
 #endif // MEMARRAY_H
