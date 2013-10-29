4eb77c4d70c28d4a445029f562f346cb304fed29
k.v <pikrat.acid@gmail.com>
2013-10-29 13:51:37 +0400

    struct field header

--- bitpacker/struct-fields.h
+++ bitpacker/struct-fields.h
@@ -7,7 +7,7 @@
     ((unsigned long)( &((const type *)0)->field ))
 
 #define field_entry( ptr, type, field ) \
-    (type *)((const char *)(ptr) - field_offset(type, field))
+    ((type *)((const char *)(ptr) - field_offset(type, field)))
 
 
 #endif // STRUCTFIELDS_H
