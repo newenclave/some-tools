17ce16cd5ecf299eea94df4e057153c1820ad46a
A <pikrat.acid@gmail.com>
2013-11-08 19:07:00 +0400

    smal

--- containers/cnt-deque.h
+++ containers/cnt-deque.h
@@ -30,33 +30,34 @@ struct cnt_deque *cnt_deque_new_reserved_pos ( size_t element_size,
                                      size_t init_reserve,
                                      enum cnt_deque_start_point position);
 
-void              cnt_deque_set_free( struct cnt_deque *cnd,
-                                      cnt_deque_element_free free_call );
 
-void              cnt_deque_free( struct cnt_deque *cnd );
-void              cnt_deque_free2( struct cnt_deque *cnd,
-                                   cnt_deque_element_free free_call );
+void    cnt_deque_set_free( struct cnt_deque *cnd,
+                            cnt_deque_element_free free_call );
 
-int               cnt_deque_empty( struct cnt_deque *cnd );
-size_t            cnt_deque_size ( struct cnt_deque *cnd );
-void             *cnt_deque_front( struct cnt_deque *cnd );
-void             *cnt_deque_back ( struct cnt_deque *cnd );
+void    cnt_deque_free( struct cnt_deque *cnd );
+void    cnt_deque_free2( struct cnt_deque *cnd,
+                         cnt_deque_element_free free_call );
 
-int               cnt_deque_pop_front ( struct cnt_deque *cnd );
-int               cnt_deque_pop_front2( struct cnt_deque *cnd,
-                                        cnt_deque_element_free free_call);
-void             *cnt_deque_create_front( struct cnt_deque *cnd);
-int               cnt_deque_push_front( struct cnt_deque *cnd, void *element );
-int               cnt_deque_push_front2(struct cnt_deque *cnd, void *element,
-                                        cnt_deque_element_copy copy_call);
+int     cnt_deque_empty( struct cnt_deque *cnd );
+size_t  cnt_deque_size ( struct cnt_deque *cnd );
+void   *cnt_deque_front( struct cnt_deque *cnd );
+void   *cnt_deque_back ( struct cnt_deque *cnd );
 
-int               cnt_deque_pop_back  ( struct cnt_deque *cnd );
-int               cnt_deque_pop_back2 ( struct cnt_deque *cnd,
-                                        cnt_deque_element_free free_call );
+int     cnt_deque_pop_front ( struct cnt_deque *cnd );
+int     cnt_deque_pop_front2( struct cnt_deque *cnd,
+                              cnt_deque_element_free free_call);
+void   *cnt_deque_create_front( struct cnt_deque *cnd);
+int     cnt_deque_push_front( struct cnt_deque *cnd, void *element );
+int     cnt_deque_push_front2(struct cnt_deque *cnd, void *element,
+                              cnt_deque_element_copy copy_call);
 
-void             *cnt_deque_create_back( struct cnt_deque *cnd);
-int               cnt_deque_push_back  ( struct cnt_deque *cnd, void *element );
-int               cnt_deque_push_back2 ( struct cnt_deque *cnd, void *element,
-                                        cnt_deque_element_copy copy_call);
+int     cnt_deque_pop_back  ( struct cnt_deque *cnd );
+int     cnt_deque_pop_back2 ( struct cnt_deque *cnd,
+                              cnt_deque_element_free free_call );
+
+void   *cnt_deque_create_back( struct cnt_deque *cnd);
+int     cnt_deque_push_back  ( struct cnt_deque *cnd, void *element );
+int     cnt_deque_push_back2 ( struct cnt_deque *cnd, void *element,
+                              cnt_deque_element_copy copy_call);
 
 #endif // CNT_DEQUE_20131111_H
