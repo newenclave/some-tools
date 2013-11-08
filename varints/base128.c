9d146c093629e17823cf6f5795723e12aa26c2d2
A <pikrat.acid@gmail.com>
2013-11-09 00:29:55 +0400

    base 128 varint

--- varints/base128.c
+++ varints/base128.c
@@ -2,14 +2,100 @@
 #include "base128.h"
 #include "../memory/mm-block.h"
 
-size_t b128_unpack( void *data, size_t lenght )
+int b128_unpack_shift( void **data, size_t *lenght, size_t *result )
 {
+    size_t   tmp    = 0;
+    int      ret    = 0;
+    unsigned shift  = 0;
+    size_t   next   = 0xFF;
+    char    *p      = (char*)*data;
+    size_t   len    = *lenght;
 
+    if( len ) do {
+        next = *p++;
+        tmp |= ((next & 0x7F) << shift);
+        shift += 7;
+    } while ( (next & 0x80) && --len );
+
+    ret = ((next & 0x80) == 0);
+
+    if( result )
+        *result = tmp;
+
+    *data   = p;
+    *lenght = len;
+
+    return ret;
 }
 
-void   b128_pack( size_t number, struct mm_block *container )
+int b128_unpack2( void *data, size_t lenght, size_t *result )
 {
-
+    return b128_unpack_shift( &data, &lenght, result );
 }
 
+int b128_unpack( struct mm_block *container, size_t *result )
+{
+    return b128_unpack2(mm_block_begin(container),
+                        mm_block_size( container ), result);
+}
 
+int b128_pack_shift( size_t number, void **container, size_t *avail )
+{
+    int   result = 0;
+    char   *data =(char *)container;
+    size_t tmp = *avail;
+    if( tmp ) {
+        if( number <= 0x7F ) {
+            *data = (char)(number & 0x7F);
+        } else {
+            char next;
+            while( number && tmp-- ) {
+                next = (unsigned char)(number & 0x7F);
+                next |= (( number >>= 7 ) ? 0x80 : 0x00 );
+                *data++ = next;
+            }
+            result = (number == 0);
+        }
+        *container = data;
+        *avail     = tmp;
+    }
+    return result;
+}
+
+int b128_pack2( size_t number, void *container, size_t avail )
+{
+    return b128_pack_shift(number, &container, &avail);
+}
+
+int b128_pack_append( size_t number, struct mm_block *container )
+{
+    int result = 0;
+    if( number <= 0x7F ) {
+        result = mm_block_push_back( container, (char)(number & 0x7F));
+    } else {
+        size_t old_size = mm_block_size( container );
+        char next;
+        result = 1;
+        while( number && result ) {
+            next = (unsigned char)(number & 0x7F);
+            next |= (( number >>= 7 ) ? 0x80 : 0x00 );
+            result = mm_block_push_back( container, next );
+        }
+        if( !result )
+            mm_block_resize( container, old_size );
+    }
+    return result;
+}
+
+int b128_pack( size_t number, struct mm_block *container )
+{
+    int result = 0;
+    struct mm_block *tmp = mm_block_new_reserved(sizeof(number));
+    if( tmp ) {
+        result = b128_pack_append( number, tmp );
+        if( result )
+            mm_block_swap( container, tmp );
+        mm_block_free( tmp );
+    }
+    return result;
+}
--- varints/base128.h
+++ varints/base128.h
@@ -5,7 +5,14 @@
 
 struct mm_block;
 
-size_t b128_unpack( void *data, size_t lenght );
-void   b128_pack( size_t number, struct mm_block *container );
+int b128_unpack( struct mm_block *container, size_t *result );
+int b128_unpack2( void *data, size_t lenght, size_t *result );
+int b128_unpack_shift( void **data, size_t *lenght, size_t *result );
+
+int b128_pack( size_t number, struct mm_block *container );
+int b128_pack2( size_t number, void *container, size_t avail );
+int b128_pack_shift( size_t number, void **container, size_t *avail );
+
+int b128_pack_append( size_t number, struct mm_block *container );
 
 #endif // BASE128_H
