ac97a317f8c25d3429e3d91d81d89afe04f03040
A <pikrat.acid@gmail.com>
2013-11-18 01:34:57 +0400

    base 128 signed

--- varints/base128.c
+++ varints/base128.c
@@ -119,8 +119,8 @@ size_t b128_pack_signed_32( int32_t number, void *container, size_t avail )
         uint32_t unumber = (uint32_t)number;
         uint8_t signbit = ((unumber & (1 << 31)) != 0);
         uint8_t next = 0;
-        unumber = signbit ? (~unumber + 1) : unumber;
 
+        unumber = signbit ? (~unumber + 1) : unumber;
         next = (uint8_t)(unumber & 0x3F) | (signbit << 6);
 
         unumber >>= 6;
@@ -130,8 +130,7 @@ size_t b128_pack_signed_32( int32_t number, void *container, size_t avail )
             next = (uint8_t)(unumber & 0x7F);
             next |= (( unumber >>= 7 ) ? 0x80 : 0x00 );
             *data++ = next;
-            used++;
         }
     }
-    return used;
+    return avail - tmp;
 }
