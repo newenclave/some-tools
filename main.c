5b0db53b07842e3b1e209d3a5bffcdfb4c67c6e5
newenclave <pikratacid@gmail.com>
2015-02-14 16:27:11 +0300

    ---

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "templ/arrays.h"
 
 array_define_type(size_t);
+array_define_type(char);
 
 #define at_index(x, y, m) ((x) * (m) + (y))
 
@@ -41,10 +42,31 @@ void fill_array( size_t_array_type *arr,
     }
 }
 
+void get_maximum_sub_DNA( const size_t_array_type *arr,
+                          char_array_type *result,
+                          const char *s1, const char *s2,
+                          const size_t n, const size_t m, const size_t M )
+{
+    const size_t *ar  = &array_at( *arr, 0 );
+
+    if( ar[at_index(n, m, M)] != 0 ) {
+        if( s1[n-1] == s2[m-1] ) {
+            get_maximum_sub_DNA(arr, result, s1, s2, n-1, m-1, M);
+            array_push_back( *result, s2[m-1] );
+        } else if( ar[at_index(n, m-1, M)] > ar[at_index(n-1, m, M)] ) {
+            get_maximum_sub_DNA(arr, result, s1, s2, n, m-1, M);
+        } else {
+            get_maximum_sub_DNA(arr, result, s1, s2, n-1, m, M);
+        }
+    }
+}
+
 int main( )
 {
     const char *DNA1 = "GCATGCCCAATTGCCGGAATTAGGCTTAG";
     const char *DNA2 = "GAATTAGGATAGGCCCAATTGCCTTGCCG";
+
+    /// result must be: 'GATGCCCAATTGCCTTGCG'
 
     const size_t L1 = strlen( DNA1 ) + 1;
     const size_t L2 = strlen( DNA2 ) + 1;
@@ -54,8 +76,11 @@ int main( )
     size_t i;
 
     size_t_array_type arr = array_init;
+    char_array_type   res = array_init;
 
     array_resize( arr, L1 * L2, err );
+    array_reserve( res, L1, err );
+
     array_foreach( arr, i ) {
         array_at( arr, i ) = 0;
     }
@@ -64,7 +89,12 @@ int main( )
 
     show_array( &arr, L1, L2 );
 
+    get_maximum_sub_DNA( &arr, &res, DNA1, DNA2, L1-1, L2-1, L2 );
+
+    printf( "Maximum subDNA is '%s'\n", &array_at(res, 0) );
+
     array_free( arr );
+    array_free( res );
 
     return 0;
 }
