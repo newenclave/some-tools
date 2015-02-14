1703e9d30498fda0e0742066ad9b75d8ee62d064
VK <kettunen.villu@gmail.com>
2015-02-15 00:06:05 +0300

    st

--- main.c
+++ main.c
@@ -3,8 +3,23 @@
 #include "templ/arrays.h"
 #include "templ/lists.h"
 
+list_define_type(int);
+
 int main( )
 {
+    int_list_type il = list_init(0);
+    int_list_type * t = &il;
+    int i;
+
+    for(i=1; i<100; i++) {
+        list_insert_value( *t, i, t );
+    }
+
+    list_foreach( il, t ) {
+        printf( "value: %d\n", t->dat_ );
+    }
+
+    list_free(il);
 
     return 0;
 }
--- templ/lists.h
+++ templ/lists.h
@@ -1,15 +1,43 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#define list_define_custom_type( type, type_name )              \
-  typedef struct type##_list_type {                             \
-      type              dat_;                                   \
-      type##_list_type *nxt_;                                   \
+#define list_define_custom_type( type, type_name )      \
+  typedef struct type##_list_type {                     \
+      struct type##_list_type  *nxt_;                   \
+      type                      dat_;                   \
   } type_name
 
 #define list_define_type( type )            \
     list_define_custom_type( type, type##_list_type )
 
-#define list_init( default_value )    { default_value, NULL }
+#define list_insert_value( lst, value, result )     \
+    do {                                            \
+        void *next__ = malloc(sizeof(lst));         \
+        if( next__ ) {                              \
+            void *tmp__ = (lst).nxt_;               \
+            (lst).nxt_       = next__;              \
+            (lst).nxt_->dat_ = value;               \
+            (lst).nxt_->nxt_ = tmp__;               \
+        }                                           \
+        result = next__;                            \
+    } while(0)
+
+#define list_delete_next( lst )             \
+    do {                                    \
+        void *next__ = (lst).nxt_;          \
+        (lst).nxt_ = (lst).nxt_->nxt_;      \
+        free(next__);                       \
+    } while(0)
+
+#define list_free(lst)                      \
+    do {                                    \
+        while( (lst).nxt_ ) {               \
+            list_delete_next(lst);          \
+        }                                   \
+    } while( 0 )
+
+#define list_foreach( lst, node ) for( node = &(lst); node; node = node->nxt_ )
+
+#define list_init( default_value )    { NULL, default_value }
 
 #endif // LISTS_H
