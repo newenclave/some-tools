fed2b2d80b224f4f70dfa31c1245b3b4728174c6
VK <kettunen.villu@gmail.com>
2015-02-14 22:46:24 +0300

    st

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
 #include "templ/arrays.h"
-
+#include "templ/lists.h"
 
 int main( )
 {
--- templ/lists.h
+++ templ/lists.h
@@ -1,6 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#define list_define_custom_type( type, type_name )              \
+  typedef struct type##_list_type {                             \
+      type              dat_;                                   \
+      type##_list_type *nxt_;                                   \
+  } type_name
 
+#define list_define_type( type )            \
+    list_define_custom_type( type, type##_list_type )
+
+#define list_init( default_value )    { default_value, NULL }
 
 #endif // LISTS_H
