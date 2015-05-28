9dcdf573c2bba9b7e3d055b8b508c48f053a8149
newenclave <pikrat.acid@gmail.com>
2015-05-28 10:36:38 +0300

    --

--- templ/lists.h
+++ templ/lists.h
@@ -40,4 +40,35 @@
 
 #define list_init( default_value )    { NULL, default_value }
 
+//////////////// Doubly linked ////////////////
+
+struct dlinked_list_head {
+    struct dlinked_list_head *links_[2];
+};
+
+#define dlinked_list_insert( src, new_l, forward )                             \
+    (&(new_l)->links_[forward!=0] = &(src)->links_[forward!=0],                \
+     &(src)  ->links_[forward!=0] = &(new_l),                                  \
+     &(new_l)->links_[forward==0] = &(src))
+
+#define dlinked_list_insert_by_field( src, new_l, field, forward )             \
+    dlinked_list_insert( &(src)->field, &(new_l)->field, forward )
+
+#define dlinked_list_remove( src )                                             \
+    (&(src)->links_[0] ?                                                       \
+     &(src)->links_[0]->links_[1] = &(src)->links_[1] : (NULL),                \
+     &(src)->links_[1] ?                                                       \
+     &(src)->links_[1]->links_[0] = &(src)->links_[0] : (NULL))
+
+#define dlinked_list_remove_by_field( src, field )                             \
+    dlinked_list_remove(&(src)->field)
+
+#define dlinked_list_step( l, forward ) ((l)->links_[forward!=0])
+
+#define dlinked_list_next( l ) dlinked_list_step( l, 0 )
+
+#define dlinked_list_prev( l ) dlinked_list_step( l, 1 )
+
+
+
 #endif // LISTS_H
